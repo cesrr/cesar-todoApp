4d84fcb34cfb202518d3b0ab90385442eb282df2
Cesar Espinoza <114450167+cesrr@users.noreply.github.com>
2024-09-16 14:27:48 -0600

    started edit functionality

--- main.js
+++ main.js
@@ -3,30 +3,47 @@ import TodoList from "./utils/todoList";
 
 const todoList = new TodoList();
 const newTodoInput = document.querySelector("#new-todo");
-const todoForm = document.querySelector("#todo-form")
-const todoView = document.querySelector("#todo-view")
+const todoForm = document.querySelector("#todo-form");
+const todoView = document.querySelector("#todo-view");
 
 const addNewTodo = () => {
   let todoText = newTodoInput.value;
   todoList.addTodo(todoText, false, Date.now);
   newTodoInput.value = "";
-  displayTodos()
-
+  displayTodos();
 };
 
 todoForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  addNewTodo()
-})
+  addNewTodo();
+});
+
+const createEditButton = () => {
+  const editButton = document.createElement("button");
+  editButton.textContent = "Edit";
+  editButton.classList.add(
+    "bg-blue-500",
+    "text-white",
+    "p-1",
+    "rounded-sm",
+    "hover:bg-blue-600"
+  );
+  return editButton
+};
 
 const displayTodos = () => {
-  todoView.innerHTML = ""
-  for(let i = 0; i < todoList.todos.length; i++){
+  todoView.innerHTML = "";
+  for (let i = 0; i < todoList.todos.length; i++) {
     const todo = todoList.todos[i];
-    const todoItem = document.createElement("li")
-    todoItem.textContent = todo.name;
-    todoView.appendChild(todoItem)
-  }
-}
 
-displayTodos()
+    const todoItem = document.createElement("li");
+
+    todoItem.textContent = todo.name;
+    todoItem.classList.add("flex", "justify-between");
+
+    todoItem.appendChild(createEditButton());
+    todoView.appendChild(todoItem);
+  }
+};
+
+displayTodos();
